f3a638ed8ee86a74194e8499911006ae3c70ad6d
Sdls31 <sdls31@gmail.com>
2024-05-10 11:41:51 -0600

    last-changes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -11,25 +11,34 @@ import CreateUser from "../components/createUser";
 import Login from "../components/login";
 import DeleteUser from "../components/deleteOrder";
 import UpdateOrders from "../components/updateOrders";
-
+import { useState } from "react";
 
 const PagesRoutes = () => {
+  const [staff, setStaff] = useState<boolean>(true);
   return (
     <>
       <NavBar />
-      <Routes>
-        
-        <Route path="/Clients" element={<Home />} />
-        <Route path="/" element={<OrdersGrid />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="/Orders" element={<Orders />} />
-        <Route path="/createorder" element={<CreateOrder />} />
-        <Route path="/createclient" element={<CreateClient />} />
-        <Route path="/createproduct" element={<CreateProduct />} />
-        <Route path="/createuser" element={<CreateUser />} />
-        <Route path="/delete" element={<DeleteUser />} />
-        <Route path="/updateOrders" element={<UpdateOrders/>} />
-      </Routes>
+
+      {staff ? (
+        <Routes>
+          {" "}
+          <Route path="/Clients" element={<Home />} />
+          <Route path="/" element={<OrdersGrid />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/Orders" element={<Orders />} />
+          <Route path="/createorder" element={<CreateOrder />} />
+          <Route path="/createclient" element={<CreateClient />} />
+          <Route path="/createproduct" element={<CreateProduct />} />
+          <Route path="/createuser" element={<CreateUser />} />
+          <Route path="/delete" element={<DeleteUser />} />
+          <Route path="/updateOrders" element={<UpdateOrders />} />
+          <Route path="/login" element={<Login setStaff={setStaff} />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<OrdersGrid />} />
+        </Routes>
+      )}
     </>
   );
 };
